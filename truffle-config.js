36551e30c385238c0a9346f11807417af5e5753e
reza1mahb <136892089+reza1mahb@users.noreply.github.com>
2024-06-12 07:44:13 +0330

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ module.exports = {
         development: {
             host: "127.0.0.1",
             port: 7545,
-            network_id: "4002"
+            network_id: "8762"
         },
         test: {
             host: "127.0.0.1",
